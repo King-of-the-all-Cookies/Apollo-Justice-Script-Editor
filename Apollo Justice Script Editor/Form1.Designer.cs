8c7728c4eca604f6d383601c883e36495f5e0ba1
King of the all Cookies <romasergienko@outlook.com>
2024-12-09 18:32:38 +0300

    Update Form1.Designer.cs

--- Apollo Justice Script Editor/Form1.Designer.cs
+++ Apollo Justice Script Editor/Form1.Designer.cs
@@ -49,9 +49,9 @@ namespace Apollo_Justice_Script_Editor
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
-            //
+            // 
             // menuStrip1
-            //
+            // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
@@ -59,9 +59,9 @@ namespace Apollo_Justice_Script_Editor
             this.menuStrip1.Size = new System.Drawing.Size(1480, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
-            //
+            // 
             // fileToolStripMenuItem
-            //
+            // 
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.openToolStripMenuItem,
             this.saveToolStripMenuItem,
@@ -69,48 +69,52 @@ namespace Apollo_Justice_Script_Editor
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
-            //
+            // 
             // openToolStripMenuItem
-            //
+            // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
             this.openToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
             this.openToolStripMenuItem.Text = "Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
-            //
+            // 
             // saveToolStripMenuItem
-            //
+            // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
             this.saveToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
             this.saveToolStripMenuItem.Text = "Save";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
-            //
+            // 
             // saveAsToolStripMenuItem
-            //
+            // 
             this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
             this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
             this.saveAsToolStripMenuItem.Text = "Save As";
             this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
-            //
+            // 
             // richTextBox1
-            //
-            this.richTextBox1.Location = new System.Drawing.Point(1060, 38);
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.richTextBox1.Location = new System.Drawing.Point(1072, 24);
             this.richTextBox1.MaxLength = 10000000;
-            this.richTextBox1.Multiline = true;
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(408, 511);
+            this.richTextBox1.Size = new System.Drawing.Size(408, 541);
             this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
-            //
+            // 
             // pictureBox1
-            //
-            this.pictureBox1.Location = new System.Drawing.Point(279, 38);
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(267, 27);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(775, 210);
+            this.pictureBox1.Size = new System.Drawing.Size(787, 221);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
-            //
+            // 
             // groupBox1
-            //
+            // 
             this.groupBox1.Controls.Add(this.button5);
             this.groupBox1.Controls.Add(this.button4);
             this.groupBox1.Controls.Add(this.textBox3);
@@ -118,100 +122,102 @@ namespace Apollo_Justice_Script_Editor
             this.groupBox1.Controls.Add(this.textBox2);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Location = new System.Drawing.Point(279, 254);
+            this.groupBox1.Location = new System.Drawing.Point(267, 254);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(406, 152);
+            this.groupBox1.Size = new System.Drawing.Size(799, 282);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Find and Replace";
-            //
+            // 
             // button5
-            //
-            this.button5.Location = new System.Drawing.Point(275, 95);
+            // 
+            this.button5.Location = new System.Drawing.Point(237, 104);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(95, 23);
             this.button5.TabIndex = 4;
             this.button5.Text = "Replace All";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
-            //
+            // 
             // button4
-            //
-            this.button4.Location = new System.Drawing.Point(162, 95);
+            // 
+            this.button4.Location = new System.Drawing.Point(136, 104);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(95, 23);
             this.button4.TabIndex = 9;
             this.button4.Text = "Replace";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
-            //
+            // 
             // textBox3
-            //
-            this.textBox3.Location = new System.Drawing.Point(18, 98);
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 104);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 5;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
-            //
+            // 
             // button3
-            //
-            this.button3.Location = new System.Drawing.Point(303, 46);
+            // 
+            this.button3.Location = new System.Drawing.Point(136, 17);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(86, 23);
+            this.button3.Size = new System.Drawing.Size(95, 23);
             this.button3.TabIndex = 8;
             this.button3.Text = "Previous";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
-            //
+            // 
             // textBox2
-            //
-            this.textBox2.Location = new System.Drawing.Point(18, 48);
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 17);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 4;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
-            //
+            // 
             // button2
-            //
-            this.button2.Location = new System.Drawing.Point(222, 45);
+            // 
+            this.button2.Location = new System.Drawing.Point(237, 17);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(89, 23);
             this.button2.TabIndex = 7;
             this.button2.Text = "Next";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
-            //
+            // 
             // button1
-            //
-            this.button1.Location = new System.Drawing.Point(141, 45);
+            // 
+            this.button1.Location = new System.Drawing.Point(196, 60);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 6;
             this.button1.Text = "Find";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            //
+            // 
             // listBoxReplies
-            //
+            // 
+            this.listBoxReplies.Dock = System.Windows.Forms.DockStyle.Left;
             this.listBoxReplies.FormattingEnabled = true;
-            this.listBoxReplies.Location = new System.Drawing.Point(12, 38);
+            this.listBoxReplies.Location = new System.Drawing.Point(0, 24);
             this.listBoxReplies.Name = "listBoxReplies";
-            this.listBoxReplies.Size = new System.Drawing.Size(261, 511);
+            this.listBoxReplies.Size = new System.Drawing.Size(261, 541);
             this.listBoxReplies.TabIndex = 5;
             this.listBoxReplies.SelectedIndexChanged += new System.EventHandler(this.listBoxReplies_SelectedIndexChanged);
-            //
+            // 
             // buttonExtractReplies
-            //
-            this.buttonExtractReplies.Location = new System.Drawing.Point(279, 526);
+            // 
+            this.buttonExtractReplies.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonExtractReplies.Location = new System.Drawing.Point(261, 542);
             this.buttonExtractReplies.Name = "buttonExtractReplies";
-            this.buttonExtractReplies.Size = new System.Drawing.Size(100, 23);
+            this.buttonExtractReplies.Size = new System.Drawing.Size(811, 23);
             this.buttonExtractReplies.TabIndex = 6;
             this.buttonExtractReplies.Text = "Extract Replies";
             this.buttonExtractReplies.UseVisualStyleBackColor = true;
             this.buttonExtractReplies.Click += new System.EventHandler(this.buttonExtractReplies_Click);
-            //
+            // 
             // Form1
-            //
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1480, 565);
@@ -231,6 +237,7 @@ namespace Apollo_Justice_Script_Editor
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
